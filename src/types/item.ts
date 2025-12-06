73959ff822ef25ca084f1c9785c5b9ab1777f8d7
Tal Mekler <tal.mekler11@gmail.com>
2025-12-06 11:18:33 +0200

    update types

--- src/types/item.ts
+++ src/types/item.ts
@@ -1,19 +1,15 @@
 import { z } from "zod";
-import { branchSchema } from "./branch";
 
 export const itemSchema = z.object({
   id: z.number().int().positive(),
   title: z.string().min(1),
   description: z.string().nullish(),
-  branch: branchSchema.pick({ id: true, title: true }),
   createdAt: z.date(),
   updatedAt: z.date().optional(),
 });
 export type TItem = z.infer<typeof itemSchema>;
 
-export const createItemSchema = itemSchema.omit({ id: true, createdAt: true, updatedAt: true, branch: true }).extend({
-  branchId: z.number().int().positive(),
-});
+export const createItemSchema = itemSchema.omit({ id: true, createdAt: true, updatedAt: true });
 export type TCreateItem = z.infer<typeof createItemSchema>;
 
 export const updateItemSchema = createItemSchema.partial();
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
+import { roleSchema } from "./role";
 
 export const userSchema = z.object({
   id: z.number().int().positive(),
   firstName: z.string().min(1),
   lastName: z.string().min(1),
   email: z.email(),
+  password: z.string().min(6),
+  role: roleSchema,
   createdAt: z.date(),
   updatedAt: z.date().optional(),
 });
